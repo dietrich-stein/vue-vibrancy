73d7655e85b300d221e0810e44255fb192baf9f8
Dietrich <dietrich.stein@gmail.com>
2020-07-05 18:52:16 +0000

    Implemented first iteration

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <Vibrancy msg="Welcome to Your Vue.js App"/>
+    <div class="demo-container">
+      <vibrancy :vibrance="0" :brightness="0" :blur="0">
+        <template v-slot:content>
+          <p>The perfect design is now within your grasp.</p>
+        </template>
+      </vibrancy>
+    </div>
   </div>
 </template>
 
@@ -16,12 +22,32 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  font-family: Helvetica, Arial, sans-serif;
+  background: url('https://images.unsplash.com/photo-1591758692416-b47871e30eac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max') no-repeat;
+  /*background: url('https://images.unsplash.com/photo-1591758692416-b47871e30eac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max') no-repeat center center fixed;*/
+  /*background: url('https://source.unsplash.com/random') no-repeat center center fixed;*/
+  /*background-size: cover;*/
+  background-attachment: fixed;
+  padding: 150px;
+}
+.demo-container {
+  width: 500px;
+  height: 200px;
+  margin: auto;
+  /*position: absolute;
+  left: 0;
+  top: 0;*/
+}
+p {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 30px;
+  font-size: 2rem;
+  color: black;
 }
 </style>
--- src/components/Vibrancy.vue
+++ src/components/Vibrancy.vue
@@ -1,58 +1,130 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="vibrancy">
+    <canvas class="vibrancy-canvas" ref="canvas" />
+    <div class="vibrancy-content">
+      <slot name="content"></slot>
+    </div>
   </div>
 </template>
 
 <script>
+import { stackBlur } from '../stackBlur'
 export default {
-  name: 'HelloWorld',
+  name: 'Vibrancy',
   props: {
-    msg: String
+    vibrance: Number,
+    brightness: Number,
+    blur: Number
+  },
+  data: function () {
+    return {
+      img: null,
+      isImageLoaded: false,
+      scrollY: 0
+    }
+  },
+  watch: {
+    isImageLoaded: function (newVal, oldVal) {
+      if (newVal === true && oldVal === false) {
+        this.img.width = this.img.naturalWidth
+        this.img.height = this.img.naturalHeight
+        this.updateCanvas()
+      }
+    }
+  },
+  methods: {
+    findParentWithImage: function (el) {
+      while (typeof(el) === 'object' && el !== null) {
+        let url = this.getImageUrl(el)
+        if (typeof(url) === 'string' && url.length > 0) {
+          return el
+        }
+        el = el.parentElement
+      }
+    },
+    getImageUrl: function(el) {
+      let elStyles = getComputedStyle(el)
+      let url = elStyles.backgroundImage.slice(4, -1).replace(/"/g, '')
+      return (url && url.length > 0) ? url : null
+    },
+    loadImage: function(url) {
+      this.isImageNeeded = true
+      this.img = new Image()
+      this.img.crossOrigin = 'Anonymous'
+      let vm = this
+      this.img.onload = function() {
+        vm.isImageLoaded = true
+      }
+      this.img.src = url
+    },
+    updateCanvas: function () {
+      // Calculate our actual component dimensions
+      var rect = this.$el.getBoundingClientRect()
+      let elWidth = Math.round(rect.right - rect.left)
+      let elHeight = Math.round(rect.bottom - rect.top)
+
+      // Set the canvas dimensions to match the component
+      this.$refs.canvas.width = elWidth
+      this.$refs.canvas.height = elHeight
+
+      let ctx = this.$refs.canvas.getContext('2d')
+      ctx.drawImage(
+        this.img,
+        this.$el.offsetLeft,               // The x coordinate where to start clipping
+        this.$el.offsetTop - this.scrollY, // The y coordinate where to start clipping
+        elWidth,                           // The width of the clipped image
+        elHeight,                          // The height of the clipped image
+        0,                                 // The x coordinate where to place the image on the canvas
+        0,                                 // The y coordinate where to place the image on the canvas
+        elWidth,                           // The width of the image to use (stretch or reduce the image)
+        elHeight                           // The height of the image to use (stretch or reduce the image)
+      )
+
+      stackBlur(this.$refs.canvas, 0, 0, elWidth, elHeight, 90);
+    }
+  },
+	created() {
+		window.addEventListener('scroll', () => {
+      this.scrollY = window.scrollY;
+      if (this.isImageLoaded) {
+        this.updateCanvas()
+      }
+		})
+	},
+  mounted: function (){
+    let parent = this.findParentWithImage(this.$el)
+    if (parent) {
+      let parentImageUrl = this.getImageUrl(parent)
+      this.loadImage(parentImageUrl)
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.vibrancy {
+  box-sizing:border-box;
+  /*background-color: blue;*/
+  position: relative;
+  padding: 0px;
+  /*display: flex;
+  flex-flow: row nowrap;
+  align-items: stretch;*/
+  height: 100%;
+  width: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.vibrancy-canvas {
+  box-sizing:border-box;
+  /*background-color: red;*/
+  opacity: 1;
+  position: absolute;
+  object-position: 0 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.vibrancy-content {
+  box-sizing:border-box;
+  position: relative;
+  /*background-color: green;*/
+  opacity: 1;
+  width: 100%;
 }
 </style>
