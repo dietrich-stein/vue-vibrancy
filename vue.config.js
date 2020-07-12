761b475edd278f592aed7e0434b81f08d3b6dc71
Dietrich <dietrich.stein@gmail.com>
2020-07-12 22:14:10 +0000

    Adds docs output for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  publicPath: '',
+  publicPath: './',
+  outputDir: './docs',
+  css: {
+    extract: false,
+    sourceMap: true
+  },
   chainWebpack: config => {
     config.module
       // Markdown loader
       .rule('md')
       .test(/\.md$/)
-
       .use('html-loader')
       .loader('html-loader')
       .end()
-
       .use('markdown-loader')
       .loader('markdown-loader')
   }
